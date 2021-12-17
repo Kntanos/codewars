326de7b05e565d3b90680c2b49eb72f22cbe29b1
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-12-17 12:10:02 +0000

    Third test pass with minimum implementation

--- js/7kyu_You_re_a_square/square.js
+++ js/7kyu_You_re_a_square/square.js
@@ -1,5 +1,5 @@
 const isSquare = (num) => {
-  if(num === 4) {
+  if(num === 4 || num == 9) {
     return true;
   } else {
     return false;
--- js/7kyu_You_re_a_square/square.test.js
+++ js/7kyu_You_re_a_square/square.test.js
@@ -10,4 +10,9 @@ describe ('isSquare', () => {
     
     expect(isSquare(5)).toBe(false);
   });
+
+  it ('returns true if number is a square', () => {
+    
+    expect(isSquare(9)).toBe(true);
+  });
 }); 
