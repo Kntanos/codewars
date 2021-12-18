235c321d68b0a18992529efe82d28f5faa795ef7
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-12-18 13:33:31 +0000

    Pass all tests

--- js/6kyu_Array_diff/arrayDiff.test.js
+++ js/6kyu_Array_diff/arrayDiff.test.js
@@ -5,4 +5,19 @@ describe ('arrayDiff', () => {
 
     expect(arrayDiff([1, 2], [1])).toEqual([2]);
   });
+
+  it ('returns [2, 3]', () => {
+
+    expect(arrayDiff([1, 2, 3], [1])).toEqual([2, 3]);
+  });
+
+  it ('returns [2, 3]', () => {
+
+    expect(arrayDiff([1, 1, 1, 2, 3], [1])).toEqual([2, 3]);
+  });
+
+  it ('returns [2, 3]', () => {
+
+    expect(arrayDiff([1, 1, 1, 2, 3], [1, 2])).toEqual([3]);
+  });
 });
