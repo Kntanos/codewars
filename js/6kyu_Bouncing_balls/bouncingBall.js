a8d3175dd4b29b4fd6160860621b98606994f46e
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-12-23 17:22:59 +0000

    Pass second test

--- js/6kyu_Bouncing_balls/bouncingBall.js
+++ js/6kyu_Bouncing_balls/bouncingBall.js
@@ -1,5 +1,11 @@
 const bouncingBall = (h, b, w) => {
-  return 1
+  if (h * b <= w) {
+    return 1 
+  } else if ( ( h * b > w ) && ( h * b ) * b <= w  )  {
+    return 3
+  } else if ( ( h * b > w ) && ( h * b ) * b > w ) {
+    return 5
+  }
 }
 
 module.exports = bouncingBall
--- js/6kyu_Bouncing_balls/bouncingBall.test.js
+++ js/6kyu_Bouncing_balls/bouncingBall.test.js
@@ -5,6 +5,16 @@ describe ('bouncingBall', () => {
   
     expect(bouncingBall(10, 0.5, 5)).toEqual(1)
   })
+
+  it ('returns 3', () => {
+  
+    expect(bouncingBall(4, 0.5, 1)).toEqual(3)
+  })
+
+  it ('returns 5', () => {
+  
+    expect(bouncingBall(6, 0.5, 1)).toEqual(5)
+  })
 })
 
 
