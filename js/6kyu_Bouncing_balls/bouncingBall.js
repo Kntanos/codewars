985c568f0da1b55f98ea063fceb24c339672935e
Kostas Ntanos <kostas@ntanos.co.uk>
2021-12-24 10:31:26 +0000

    Pass tests for the correct loop

--- js/6kyu_Bouncing_balls/bouncingBall.js
+++ js/6kyu_Bouncing_balls/bouncingBall.js
@@ -1,29 +1,14 @@
-// const bouncingBall = (h, b, w) => {
-//   if (h * b <= w) {
-//     return Math.ceil((h * b)/w)
-//   } else if ( ( h * b > w ) && ( h * b ) * b <= w  )  {
-//     return 3
-//   } else if ( ( h * b > w ) && ( h * b ) * b >= w ) {
-//     return 5
-//   } else if ( ( h * b > w ) )
-// }
-
-
-
 const bouncingBall = (h, b, w) => {
 let bounces = 0
 if (h * b <= w) {
   bounces++
-  } else if ( h * b > w ) {
-  if ( ( h * b ) * b <= w ) {
-    bounces = bounces +3
-  } else if ( ( h * b ) * b >= w ) {
-    bounces = bounces +5
+} else {
+    bounces++
+    for ( let height = h * b ; height > w ; height = height * b ){
+      bounces = bounces + 2
+    }
   }
-
 return bounces
 }
-}
-
 
 module.exports = bouncingBall
