df56b311d3780c15bd800badc0736ac7b847de6e
Kostas Ntanos <kostas@ntanos.co.uk>
2021-12-24 09:35:37 +0000

    Half way doing something

--- js/6kyu_Bouncing_balls/bouncingBall.js
+++ js/6kyu_Bouncing_balls/bouncingBall.js
@@ -1,11 +1,29 @@
+// const bouncingBall = (h, b, w) => {
+//   if (h * b <= w) {
+//     return Math.ceil((h * b)/w)
+//   } else if ( ( h * b > w ) && ( h * b ) * b <= w  )  {
+//     return 3
+//   } else if ( ( h * b > w ) && ( h * b ) * b >= w ) {
+//     return 5
+//   } else if ( ( h * b > w ) )
+// }
+
+
+
 const bouncingBall = (h, b, w) => {
-  if (h * b <= w) {
-    return 1 
-  } else if ( ( h * b > w ) && ( h * b ) * b <= w  )  {
-    return 3
-  } else if ( ( h * b > w ) && ( h * b ) * b > w ) {
-    return 5
+let bounces = 0
+if (h * b <= w) {
+  bounces++
+  } else if ( h * b > w ) {
+  if ( ( h * b ) * b <= w ) {
+    bounces = bounces +3
+  } else if ( ( h * b ) * b >= w ) {
+    bounces = bounces +5
   }
+
+return bounces
 }
+}
+
 
 module.exports = bouncingBall
