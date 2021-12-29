217c7f1c267317d1e48169f5e7b1ab82790180fb
Kostas Ntanos <kostas@ntanos.co.uk>
2021-12-29 14:04:59 +0000

    Refactor

--- js/6kyu_Spinning_words/spinWords.js
+++ js/6kyu_Spinning_words/spinWords.js
@@ -1,11 +1,6 @@
 const spinWords = (string) => {
   return string.split(" ").map((word) => { 
-
-    if (word.length >= 5) { 
-      return word.split("").reverse().join("")}
-    else {
-      return word
-    }
+    return word.length >= 5 ? word.split("").reverse().join("") : word
   }).join(" ")
 }
 
