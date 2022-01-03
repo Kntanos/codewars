cca930b8db1670e7b68aeb42c9f4fbb2ae41093e
Kostas Ntanos <kostas@ntanos.co.uk>
2022-01-03 11:04:21 +0000

    Pass third test

--- js/6kyu_Consecutive_strings/consecutive_strings.js
+++ js/6kyu_Consecutive_strings/consecutive_strings.js
@@ -1,7 +1,8 @@
 const longestConsec = (strarr, k) => {
   let array = []
   for (let i = 0; i <= (strarr.length - k); i++) {
-    array.push(strarr[i].concat(strarr[i+1]))
+    // array.push(strarr[i].concat(strarr[i+1]))
+    array.push(strarr.slice(i, i+k).join(''))
   }
   return array
 }
--- js/6kyu_Consecutive_strings/consecutive_strings.test.js
+++ js/6kyu_Consecutive_strings/consecutive_strings.test.js
@@ -3,7 +3,8 @@ const longestConsec = require ('./consecutive_strings')
 describe('longestConsec', () => {
   it('concatenates k consecutive strings', () => {
 
-    // expect(longestConsec(['one', 'two'])).toEqual('onetwo')
+    expect(longestConsec(['one', 'two'], 2)).toEqual(['onetwo'])
     expect(longestConsec(['one', 'two', 'three'], 2)).toEqual(['onetwo', 'twothree'])
+    expect(longestConsec(['one', 'two', 'three'], 3)).toEqual(['onetwothree'])
   })
 })
