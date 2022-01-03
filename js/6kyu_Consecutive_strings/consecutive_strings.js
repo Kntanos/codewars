f03e84e7dff1b0dbb7b7d19de9b548d6f9fc7cc5
Kostas Ntanos <kostas@ntanos.co.uk>
2022-01-03 11:30:43 +0000

    Pass fourth test

--- js/6kyu_Consecutive_strings/consecutive_strings.js
+++ js/6kyu_Consecutive_strings/consecutive_strings.js
@@ -1,10 +1,11 @@
 const longestConsec = (strarr, k) => {
   let array = []
   for (let i = 0; i <= (strarr.length - k); i++) {
-    // array.push(strarr[i].concat(strarr[i+1]))
-    array.push(strarr.slice(i, i+k).join(''))
+    array.push(strarr.slice(i, i + k).join(''))
   }
-  return array
+  return array.sort((a, b) => {
+    return b.length - a.length 
+  })[0]
 }
 
 module.exports = longestConsec
--- js/6kyu_Consecutive_strings/consecutive_strings.test.js
+++ js/6kyu_Consecutive_strings/consecutive_strings.test.js
@@ -3,8 +3,11 @@ const longestConsec = require ('./consecutive_strings')
 describe('longestConsec', () => {
   it('concatenates k consecutive strings', () => {
 
-    expect(longestConsec(['one', 'two'], 2)).toEqual(['onetwo'])
-    expect(longestConsec(['one', 'two', 'three'], 2)).toEqual(['onetwo', 'twothree'])
-    expect(longestConsec(['one', 'two', 'three'], 3)).toEqual(['onetwothree'])
+    expect(longestConsec(['one', 'two'], 2)).toEqual('onetwo')
+    expect(longestConsec(['one', 'two', 'three'], 2)).toEqual('twothree')
+    expect(longestConsec(['one', 'two', 'three'], 3)).toEqual('onetwothree')
+    expect(longestConsec(['one', 'two', 'three', 'four'], 2)).toEqual('threefour')
+    expect(longestConsec(['one', 'two', 'three', 'four'], 2)).toEqual('threefour')
+    expect(longestConsec(['one', 'two', 'three', 'four'], 3)).toEqual('twothreefour')
   })
 })
