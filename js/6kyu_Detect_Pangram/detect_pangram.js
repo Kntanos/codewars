292ed2e8bd6a4318b289b52338194a58fe7cc9c6
Kostas Ntanos <kostas@ntanos.co.uk>
2022-01-08 21:21:10 +0000

    Pass third test

--- js/6kyu_Detect_Pangram/detect_pangram.js
+++ js/6kyu_Detect_Pangram/detect_pangram.js
@@ -1,6 +1,6 @@
 const isPangram = (str) => {
   const alphabet = ('abcdefghijklmnopqrstuvwxyz').split('')
-  return alphabet.every(letter => str.split('').includes(letter))
+  return alphabet.every(letter => str.toLowerCase().split('').includes(letter))
 }
 
 module.exports = isPangram
--- js/6kyu_Detect_Pangram/detect_pangram.test.js
+++ js/6kyu_Detect_Pangram/detect_pangram.test.js
@@ -2,9 +2,13 @@ const isPangram = require('./detect_pangram')
 
 describe('isPangram', () => {
   it('returns true if str is Pangram', () => {
-    let str = 'abcdefghijklmnopqrstuvwxyz'
+    let str1 = 'abcdefghijklmnopqrstuvwxyz'
+    let str2 = 'abcde33fghi jklmn4opq rstuv33wxyz'
+    let str3 = 'aBcdefghi jkLmnopq rstuvwxyz'
 
-    expect(isPangram(str)).toBe(true)
+    expect(isPangram(str1)).toBe(true)
+    expect(isPangram(str2)).toBe(true)
+    expect(isPangram(str3)).toBe(true)
   })
 
   it('returns false if str is not Pangram', () => {
