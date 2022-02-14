1dcbca23e19d2c88b688bf732c0313f396744f4c
Kostas Ntanos <kostas@ntanos.co.uk>
2022-02-14 11:36:52 +0000

    Pass 5th test general solution

--- js/6kyu_Find_the_odd/findOdd.js
+++ js/6kyu_Find_the_odd/findOdd.js
@@ -3,7 +3,17 @@ function findOdd(A) {
     return A[0];
   }
 
-  return A.find (i => i != 0)
+  const counts = {};
+  
+  A.forEach((x) => {
+    counts[x] = (counts[x] || 0) + 1;
+  });
+
+  for (const [k, v] of Object.entries(counts)) {
+    if (v % 2 !=0) {
+      return parseInt(k)
+    }
+  }
 }
 
 module.exports = findOdd
