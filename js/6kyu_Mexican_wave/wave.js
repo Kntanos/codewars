5a439f78dcb8ba961313fb2def52095190933150
Kostas Ntanos <kostas@ntanos.co.uk>
2022-02-18 13:14:55 +0000

    Refactor

--- js/6kyu_Mexican_wave/wave.js
+++ js/6kyu_Mexican_wave/wave.js
@@ -1,14 +1,10 @@
 const wave = (str) => {
   if (str.length === 0) {
     return [str]
-  } else if (str.length === 1) {
-    return [str.toUpperCase()]
-  } else if (str.length === 2) {
+  } else {
     return str.split('').map(char => 
       str.replace(char, char.toUpperCase())
     )
-    // return [str.replace(str[0], str[0].toUpperCase()), str.replace(str[1], str[1].toUpperCase()) ]
-    // return [str[0].toUpperCase() + str[1], str[0] + str[1].toUpperCase()]
   }
 }
 
